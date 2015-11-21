87a282ceeeb1a5c8db8a2270908b1dd1163669e5
Rasie1 <rasielll@gmail.com>
2015-11-21 18:56:14 +0300

    Death and animations

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 
 var PLAYER_WIDTH = 48;
 var PLAYER_HEIGHT = 62;
-var MONSTER_SPEED = 64;
+var MONSTER_SPEED = 164;
 var MONSTER_DIRECTION_CHANGE_CHANCE = 0.3;
 var MONSTER_AWARENESS_RANGE = 128;
 var MONSTER_UPDATE_TIME = 64;
@@ -58,7 +58,16 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
     player.animations.play('right'); //Constantly "moving" to the right 
 
     monsters.map( function(monster) { 
-      game.physics.arcade.collide(monster, platforms);
+      if (!monster.dead) {
+        game.physics.arcade.collide(monster, platforms);
+        if (Phaser.Rectangle.intersects(player.getBounds(), monster.getBounds())) {
+          monster.dead = true;
+          monster.enableBody = false;
+          var animation = (Math.random() < 0.5) ? monsterDeathAnimationSplat
+                                                : monsterDeathAnimationFall;
+          monster.die(animation);
+        }
+      }
     });
 
     //  Allow the player to jump if they are touching the ground.
@@ -101,13 +110,64 @@ function createSky() {
   game.add.sprite(0, 0, 'sky');
 }
 
+function removeMonster(monster) {
+  var i = monsters.indexOf(monster);
+  monsters.splice(i, 1);
+  monster.destroy();
+}
+
+function monsterDeathAnimationSplat(monster)
+{
+  var disappearDelay = 1600;
+  monster.scale.y = 0.3;
+  monster.scale.x = 1.7;
+  monster.body.velocity.x = -LEVEL_SPEED;
+  monster.body.velocity.y = 0;
+  monster.body.immovable = true;
+  monster.body.gravity.y = 0;
+  // fade out
+  game.add.tween(monster).to({ alpha: 0 }, 
+                             disappearDelay, 
+                             Phaser.Easing.Linear.None, 
+                             true, 
+                             0, 
+                             1000, 
+                             true);
+  timer = game.time.create(false);
+  timer.add(disappearDelay,
+            function() { 
+              removeMonster(monster);
+            });
+  timer.start();
+}
+
+function monsterDeathAnimationFall(monster) {
+  var disappearDelay = 600;
+  // fade out
+  game.add.tween(monster).to({ alpha: 0 }, 
+                             disappearDelay, 
+                             Phaser.Easing.Linear.None, 
+                             true, 
+                             0, 
+                             1000, 
+                             true);
+
+  timer = game.time.create(false);
+  timer.add(disappearDelay,
+            function() { 
+              removeMonster(monster);
+            });
+  timer.start();
+}
+
+
 function createMonster() {
   monster = game.add.sprite(game.world.width, 
                             0, 
                             'monster');
 
   game.physics.arcade.enable(monster); 
-  monster.body.bounce.y = 0;
+  monster.body.bounce.y  = 0;
   monster.body.gravity.y = 1000;
   monster.body.collideWorldBounds = true;
 
@@ -120,9 +180,11 @@ function createMonster() {
     return (Math.random() < MONSTER_DIRECTION_CHANGE_CHANCE)
   };
 
-  monster.knockOut = function() {
-    // this.body.angularVelocity = 3;
-    this.body.velocity.y += 10;
+  monster.dead = false;
+  monster.die = function(animation) {
+    this.body.velocity.x = 0;
+    this.dead.true;
+    animation(this);
   };
 
   monsters.push(monster);
@@ -132,6 +194,8 @@ function createMonster() {
 
 
 function updateMonster(monster) {
+  if (monster.dead)
+    return;
   if (Math.random() < MONSTER_DIRECTION_CHANGE_CHANCE)
   {
     monster.direction = !(monster.direction);
@@ -142,16 +206,13 @@ function updateMonster(monster) {
         monster.body.velocity.x = -MONSTER_SPEED - LEVEL_SPEED;
     }
   }
-
-  // if (collide(player, monster)) {
-  //   monster.knockOut();
-  // }
 }
+
+
 
 function updateMonsters() {
   // Spawn new monsters
-  if (Math.random() < MONSTER_SPAWN_CHANCE)
-  {
+  if (Math.random() < MONSTER_SPAWN_CHANCE) {
     createMonster();
   }
   monsters.map(updateMonster);
@@ -163,7 +224,7 @@ function createPlayer() {
   game.physics.arcade.enable(player); // We need to enable physics on the player
 
   //  Player physics properties
-  player.body.bounce.y = 0;
+  player.body.bounce.y  = 0;
   player.body.gravity.y = 1000;
   player.body.collideWorldBounds = true;
 
