fa66ea69eecdfadbd582eb2a6f6b02294aba5e0e
geo2a <georgiylukjanov@gmail.com>
2015-11-21 17:26:54 +0300

    Косячная версия телепортации статзиллы (сверху-снизу) по нажатию пробела

--- js/main.js
+++ js/main.js
@@ -6,12 +6,17 @@
 var PLAYER_WIDTH = 48;
 var PLAYER_HEIGHT = 62;
 
+var GROUND_WIDTH = 800;
+var GROUND_HEIGHT = 32;
+
+var GRAVITY = 500;
 ////////////////////////
 // Глобальные объекты //
 ////////////////////////
 var player;
 var platforms;
 var spacebar;
+var ground;
 
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { 
@@ -22,6 +27,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
                           PLAYER_WIDTH, PLAYER_HEIGHT); 
   }, 
   create: function() {
+
     //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
     
@@ -32,6 +38,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
     // Our controls.
     spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     spacebar.onDown.add(flipGravity);
+
   }, 
   update: function() {
     game.physics.arcade.collide(player, platforms);//Collide player and ground
@@ -45,22 +52,33 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
   render: function() {
     game.debug.cameraInfo(game.camera, 32, 32);
     game.debug.spriteCoords(player, 32, 500);
+
   }
 });
 
 function flipGravity() {
-  player.position.y += PLAYER_HEIGHT;
+  console.log("SPACEBAR pressed: gravity flip");
+  console.log("Y before gravity flip: " + player.body.position.y);
   player.body.gravity.y = -player.body.gravity.y; 
-  // player.scale.y *= -1; // зеркально отобразить спрайт относительно y
+  var new_y = 0;
+  if (player.body.gravity.y > 0) {
+    new_y = ground.body.position.y - 100;
+  } else {
+    new_y = ground.body.position.y + 100;
+  }
+  player.reset(player.body.position.x, new_y);
+  console.log("Y after gravity flip: " + player.body.position.y);
+  player.scale.y *= -1; // зеркально отобразить спрайт относительно y
 }
 
 //  The platforms group contains the ground
 function createPlatforms() {
   platforms = game.add.group();
   platforms.enableBody = true; //  We will enable physics for any object that is created in this group
-
-  var ground = platforms.create(0, game.world.height / 2, 'ground'); // Here we create the ground. 
+  ground = 
+    platforms.create(0, game.world.height / 2, 'ground'); // Here we create the ground. 
   ground.scale.setTo(2, 1); //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+  // ground.scale(GROUND_WIDTH, GROUND_HEIGHT)
   ground.body.immovable = true; //  This stops it from falling away when you jump on it
 }
 
@@ -76,7 +94,7 @@ function createPlayer() {
 
   //  Player physics properties
   player.body.bounce.y = 0;
-  player.body.gravity.y = 1000;
+  player.body.gravity.y = GRAVITY;
   player.body.collideWorldBounds = true;
 
   //  Animations, walking left and right.
@@ -84,6 +102,10 @@ function createPlayer() {
   player.animations.add('right', [8, 9, 10, 11], 10, true);
 }
 
-function createHisto() {
+function setPlayerGravityNormal() {
+  player.body.gravity = GRAVITY;
+}
 
+function setPlayerGravityReverse() {
+  player.body.gravity = -GRAVITY;
 }
