5767e6ca47bc4d30f4aa13db7cf22ac8bc5d9eb4
Anna <amuhar3@mail.ru>
2015-11-22 15:19:16 +0300

    add objects

--- js/main.js
+++ js/main.js
@@ -14,14 +14,11 @@ function PRELOAD() {
   game.load.spritesheet('dino', 'assets/dinosprite2.png', PLAYER_WIDTH, PLAYER_HEIGHT); 
 
     game.load.image('histo', 'assets/histo.svg');
-    game.load.image('obj1', 'assets/obj1.png');
-    game.load.image('obj2', 'assets/obj2.png');
-    game.load.image('obj3', 'assets/obj3.png');
-    game.load.image('obj4', 'assets/obj4.png');
-    game.load.image('obj5', 'assets/obj5.png');
-    game.load.image('obj6', 'assets/obj6.png');
-    game.load.image('obj7', 'assets/obj7.png');
-    game.load.image('obj8', 'assets/obj8.png');
+
+    for (var i = 1; i < 15; i++) {
+        game.load.image('obj' + i, 'assets/obj' + i + '.png');
+    }
+
 }
 
 function CREATE() {
@@ -46,10 +43,11 @@ function UPDATE() {
     player.animations.play('right'); //Constantly "moving" to the right 
     game.physics.arcade.collide(player, histo, collisionHandler, null, this);
     updateMonstersPerTick();
-   // object's moving
+    // object's moving
 
-   movingObjects(obj);
     
+    movingObjects(obj);
+
 }
 
 function RENDER() {
